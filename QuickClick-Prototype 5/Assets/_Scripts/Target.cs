d2da32401d97ed0d50a64cd60a93ced2c7c54a67
Bryan-Codegame <71522312+Bryan-Codegame@users.noreply.github.com>
2021-04-01 11:45:56 -0600

    Add rididbody force and torque

--- QuickClick-Prototype 5/Assets/_Scripts/Target.cs
+++ QuickClick-Prototype 5/Assets/_Scripts/Target.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    public float maxForce = 20.0f, minForce = 15.0f;
+    public float torque = 12.0f;
+    public float xRange = 4;
+    public float yStartSpawn = -5;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _rigidbody.AddForce(RandomForceDirection(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    /// <summary>
+    /// Generate a random vector
+    /// <summary/>
+    /// <returns>Upward random force <returns/>
+    private Vector3 RandomForceDirection()
+    {
+        return Vector3.up*Random.Range(minForce, maxForce);
+    }
+
+    /// <summary>
+    /// Generate a random torque
+    /// <summary/>
+    /// <returns> Random value between torque  <returns/>
+    private float RandomTorque()
+    {
+        return Random.Range(-torque, torque);
+    }
+
+    /// <summary>
+    /// Generate a random vector
+    /// <summary/>
+    /// <retuns>Vector with random x position and y position to spawn <returns/>
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yStartSpawn);
     }
 }
